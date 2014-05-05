441981784d731c3ba349cc189e56fed2ab182d3f
etheleon <picy2k@gmail.com>
2014-05-05 10:16:46 +0800

    done

--- metamaps/GLO/scripts/m8-graph-3.r
+++ metamaps/GLO/scripts/m8-graph-3.r
@@ -2,4 +2,17 @@
 library(dplyr)
 
 args<-commandArgs(T)
-read.table(args[1], sep="\t",comment.char="")
+#args=c('example/outputs/m8-graph-2_output')
+
+data=read.table(args[1], sep="\t",comment.char="",h=T)
+data=data[complete.cases(data),]
+data=tbl_df(data)
+
+data %.%
+group_by(READ) %.% 
+
+data2=setNames(ddply(data, "READ", function(x) { 
+GLO=paste(sort(unique(as.integer(x$DESIRED_RANKID))), collapse="_")
+}), c("READ", "GLO"))
+
+write.table(data2, file=args[2], quote=F, row.names=F)
